1ca5b0ca11a662eff148201e5ce838f472b9308d
Ernesto Arevalo <ernestoarevalo@gmail.com>
2026-06-26 09:08:51 -0300

    Corregir firebase-config con sintaxis compat

--- static/js/firebase-config.js
+++ static/js/firebase-config.js
@@ -1,21 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// static/js/firebase-config.js
+// -----------------------------------------------------------------
+// PEGA AQUÍ las credenciales que te da la consola de Firebase
+// (Configuración del proyecto > Tus apps > SDK de Firebase).
+// Ver instrucciones en el README para obtener estos valores.
+// -----------------------------------------------------------------
+ 
 const firebaseConfig = {
   apiKey: "AIzaSyBW-p6OX_HOvplLvE7T74uc5XSlt0D332w",
   authDomain: "gym-app-73834.firebaseapp.com",
   projectId: "gym-app-73834",
   storageBucket: "gym-app-73834.firebasestorage.app",
   messagingSenderId: "941031909123",
-  appId: "1:941031909123:web:b43faf33d53281f0fa2b36",
-  measurementId: "G-47FFNXPX4Y"
+  appId: "1:941031909123:web:b43faf33d53281f0fa2b36"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+ 
+// Inicializa Firebase (usando los SDKs cargados vía <script> en el HTML)
+firebase.initializeApp(firebaseConfig);
+ 
+const auth = firebase.auth();
+const db = firebase.firestore();
